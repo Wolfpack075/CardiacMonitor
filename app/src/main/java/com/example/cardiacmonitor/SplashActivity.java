b0248c69a936e9802c7b24f2baedae4cc8bfd7ce
Kazi <kazifahimtahmid@gmail.com>
2023-07-06 00:11:02 +0600

    Splash Screen upload

---
+++ app/src/main/java/com/example/cardiacmonitor/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.cardiacmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.cardiacmonitor.R;
+
+/**
+ * App opening splash screen.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    /**
+     * oncreate method for splash screen
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        try{
+            getSupportActionBar().hide();
+
+        }catch (Exception e)
+        {
+
+        }
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }}
